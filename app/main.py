e83f6030a70539d49e5bf231888d2f275d3ac1c9
ROHITHC1526 <rohithmani133@gmail.com>
2026-03-05 01:15:19 +0530

    fix cors for vercel frontend

--- app/main.py
+++ app/main.py
@@ -18,9 +18,14 @@ logging.basicConfig(level=logging.INFO)
 app = FastAPI(title="Hackathon Registration API")
 
 # CORS
+origins = [
+    "https://techxelerate2026csm.vercel.app",
+    "http://localhost:3000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
